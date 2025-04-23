6155c41b7a3b80721a85d9efca6ed9a90cc26f35
Andrew <andrewkim102@ucsb.edu>
2025-04-23 02:36:45 -0700

    Added @Entity RecommendationRequest

---
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -0,0 +1,37 @@
+package edu.ucsb.cs156.example.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.lang.annotation.Inherited;
+import java.time.LocalDateTime;
+
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+// table name in the database
+@Entity(name = "RecommendationRequest") 
+
+
+
+public class RecommendationRequest {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+    private String requestorEmail;
+    private String professorEmail;
+    private String explanation;    // Would I need a limitation of length here?
+    
+    private LocalDateTime dateRequested;
+    private LocalDateTime dateNeeded;
+
+    private boolean done;
+
+}
